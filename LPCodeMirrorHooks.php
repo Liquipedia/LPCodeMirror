39eddbf567da84fb71a26a283838cd0d364f617c
FO-nTTaX <fo.nttax@yahoo.de>
2018-07-05 17:32:43 +0200

    array syntax

--- LPCodeMirrorHooks.php
+++ LPCodeMirrorHooks.php
@@ -6,33 +6,33 @@ class Hooks {
 
 	public static function onGetPreferences( $user, &$preferences ) {
 		// CodeMirror settings
-		$preferences[ 'lpcodemirror-prefs-use-codemirror-phone' ] = array(
+		$preferences[ 'lpcodemirror-prefs-use-codemirror-phone' ] = [
 			'type' => 'check',
 			'label-message' => 'lpcodemirror-prefs-use-codemirror-phone',
 			'section' => 'editing/lpcodemirror'
-		);
-		$preferences[ 'lpcodemirror-prefs-use-codemirror-tablet' ] = array(
+		];
+		$preferences[ 'lpcodemirror-prefs-use-codemirror-tablet' ] = [
 			'type' => 'check',
 			'label-message' => 'lpcodemirror-prefs-use-codemirror-tablet',
 			'section' => 'editing/lpcodemirror'
-		);
-		$preferences[ 'lpcodemirror-prefs-use-codemirror' ] = array(
+		];
+		$preferences[ 'lpcodemirror-prefs-use-codemirror' ] = [
 			'type' => 'check',
 			'label-message' => 'lpcodemirror-prefs-use-codemirror',
 			'section' => 'editing/lpcodemirror'
-		);
-		$preferences[ 'lpcodemirror-prefs-use-codemirror-linewrap' ] = array(
+		];
+		$preferences[ 'lpcodemirror-prefs-use-codemirror-linewrap' ] = [
 			'type' => 'check',
 			'label-message' => 'lpcodemirror-prefs-use-codemirror-linewrap',
 			'section' => 'editing/lpcodemirror'
-		);
+		];
 	}
 
 	public static function onMakeGlobalVariablesScript( array &$vars, \OutputPage $out ) {
 		$config = $out->getConfig();
 		$context = $out->getContext();
 		// add CodeMirror vars only for edit pages
-		if ( in_array( $context->getRequest()->getText( 'action' ), array( 'edit', 'submit' ) ) ) {
+		if ( in_array( $context->getRequest()->getText( 'action' ), [ 'edit', 'submit' ] ) ) {
 			$parser = $config->get( 'Parser' );
 			$contObj = $context->getLanguage();
 
@@ -42,21 +42,21 @@ class Hooks {
 			}
 
 			// initialize global vars
-			$vars += array(
-				'LPCodemirrorExtModes' => array(
-					'tag' => array(
+			$vars += [
+				'LPCodemirrorExtModes' => [
+					'tag' => [
 						'pre' => 'mw-tag-pre',
 						'nowiki' => 'mw-tag-nowiki',
-					),
-					'func' => array(),
-					'data' => array(),
-				),
+					],
+					'func' => [],
+					'data' => [],
+				],
 				'LPCodemirrorTags' => array_fill_keys( $parser->getTags(), true ),
-				'LPCodemirrorDoubleUnderscore' => array( array(), array() ),
+				'LPCodemirrorDoubleUnderscore' => [ [], [] ],
 				'LPCodemirrorFunctionSynonyms' => $parser->mFunctionSynonyms,
 				'LPCodemirrorUrlProtocols' => $parser->mUrlProtocols,
 				'LPCodemirrorLinkTrailCharacters' => $contObj->linkTrail(),
-			);
+			];
 
 			$mw = $contObj->getMagicWords();
 			foreach ( \MagicWord::getDoubleUnderscoreArray()->names as $name ) {
