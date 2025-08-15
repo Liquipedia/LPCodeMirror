1d4e77fc9d87d780da6192d8cd27158a7cf69304
SyntacticSalt <syntactic@mbergen.de>
2025-08-15 12:56:59 +0200

    add semicolon

--- resources/scripts/codemirror.js
+++ resources/scripts/codemirror.js
@@ -383,7 +383,7 @@ if ( !String.prototype.includes ) {
 			} else if ( pagename.startsWith( 'widget=' ) ) {
 				// Support invokes of style
 				// {{#invoke:Lua|invoke|module=Widget/Factory|fn=fromTemplate|widget=pagename}}
-				pagename = pagename.replace( 'widget=', 'Widget/' )
+				pagename = pagename.replace( 'widget=', 'Widget/' );
 			} else if ( !parserfunction.hasClass( 'cm-mw-parserfunction-name' ) ||
 				parserfunction.text() !== '#invoke' ) {
 				return;
